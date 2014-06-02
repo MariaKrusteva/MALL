a10d2c5179c416a369bec0f06373cb7cf7ed130b
Maria Krusteva <mimito_68@abv.bg>
2014-06-02 11:39:02 +0300

    Fixing the Store class

--- store.py
+++ store.py
@@ -21,19 +21,19 @@ class Store:
         print("If you want to shop here please insert 2")
 
         choice = input("+++")
-        if(choice == 1):
+        if(choice == "1"):
             print("""For loading the sold out items insert 1,
                      for deleting the store insert 2
                      for adding items insert 3""")
             staff_choice = input("+++")
-            if(staff_choice == 1):
+            if(staff_choice == "1"):
                 quantity = input("quantity: ")
                 store_sql_manager.load_items(quantity)
 
-            elif(staff_choice == 2):
+            elif(staff_choice == "2"):
                 store_sql_manager.delete_store(self.name)
 
-            elif(staff_choice == 3):
+            elif(staff_choice == "3"):
                 item = input("item: ")
                 description = input("description: ")
                 providers_price = input("provider's price: ")
@@ -45,17 +45,17 @@ class Store:
             else:
                 print("invalid command")
 
-        elif (choice == 2):
+        elif (choice == "2"):
             print("to see all the items in the store - 1")
             print("to see item with actual id - 2")
             print("to buy item - 3")
             buyer_choice = input("+++")
-            if(buyer_choice == 1):
+            if(buyer_choice == "1"):
                 store_sql_manager.view_items()
-            elif(buyer_choice == 2):
+            elif(buyer_choice == "2"):
                 id = input("id: ")
                 store_sql_manager.view_item(id)
-            elif(buyer_choice == 3):
+            elif(buyer_choice == "3"):
                 item = input("item you want to buy: ")
                 store_sql_manager.sell_item(self.name, item)
                 print("---you just bought {}---".format(item))
