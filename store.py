bf5683018bc8ad84fcfba076be4121e05f639c06
Maria Krusteva <mimito_68@abv.bg>
2014-06-22 00:52:45 +0300

    working Store module

--- store.py
+++ store.py
@@ -15,52 +15,108 @@ class Store:
     def name(self, value):
         self._name = value
 
-    def enter_the_store(self):
-        print("Welcome to {} !".format(self.name))
-        print("If you are part of the staff please insert 1")
-        print("If you want to shop here please insert 2")
+    def is_part_of_the_staff(self):
+        username = input("username: ")
+        password = input("password: ")
+        valid = store_sql_manager.get_staff_information(self.name)
+        return username == valid[0] and password == valid[1]
 
-        choice = input("+++")
-        if(choice == "1"):
-            print("""For loading the sold out items insert 1,
-                     for deleting the store insert 2
-                     for adding items insert 3""")
-            staff_choice = input("+++")
-            if(staff_choice == "1"):
-                quantity = input("quantity: ")
-                store_sql_manager.load_items(quantity)
+    def staff_menu(self):
+        print("""for loading the sold out items insert 1,
+                 for deleting the store insert 2
+                 for adding items insert 3
+                 if you want to exit insert 4""")
 
-            elif(staff_choice == "2"):
-                store_sql_manager.delete_store(self.name)
+        staff_choice = input("+++")
 
-            elif(staff_choice == "3"):
-                item = input("item: ")
-                description = input("description: ")
-                providers_price = input("provider's price: ")
-                selling_price = input("selling price: ")
-                quantity = input("quantity: ")
+        if staff_choice == "1":
+            quantity = input("quantity: ")
+            store_sql_manager.load_items(quantity)
 
-                store_sql_manager.add_item(self.name, item, description, providers_price, selling_price, quantity)
+        elif staff_choice == "2":
+            store_sql_manager.delete_store(self.name)
 
-            else:
-                print("invalid command")
+        elif staff_choice == "3":
+            item = input("item: ")
+            description = input("description: ")
+            providers_price = input("provider's price: ")
+            selling_price = input("selling price: ")
+            quantity = input("quantity: ")
 
-        elif (choice == "2"):
-            print("to see all the items in the store - 1")
-            print("to see item with actual id - 2")
-            print("to buy item - 3")
-            buyer_choice = input("+++")
-            if(buyer_choice == "1"):
-                store_sql_manager.view_items()
-            elif(buyer_choice == "2"):
-                id = input("id: ")
-                store_sql_manager.view_item(id)
-            elif(buyer_choice == "3"):
-                item = input("item you want to buy: ")
-                store_sql_manager.sell_item(self.name, item)
-                print("---you just bought {}---".format(item))
-            else:
-                print("invalid command")
+            store_sql_manager.add_item(
+                self.name,
+                item,
+                description,
+                providers_price,
+                selling_price,
+                quantity
+            )
+
+        elif staff_choice == "4":
+            return False
 
         else:
             print("invalid command")
+
+        return True
+
+    def buyer_menu(self):
+
+        print("""to see all the items in the store insert 1
+                 to see item with actual id insert 2
+                 to buy item insert 3
+                 if you want to exit insert 4""")
+
+        buyer_choice = input("+++")
+
+        if buyer_choice == "1":
+            store_sql_manager.view_items()
+
+        elif buyer_choice == "2":
+            id = input("id: ")
+            store_sql_manager.view_item(id)
+
+        elif buyer_choice == "3":
+            item = input("item you want to buy: ")
+            store_sql_manager.sell_item(self.name, item)
+            print("---you just bought {}---".format(item))
+
+        elif buyer_choice == "4":
+            return False
+
+        else:
+            print("invalid command")
+
+        return True
+
+    def enter_the_store(self):
+        store_sql_manager.create_staff_table()
+        store_sql_manager.create_table()
+        store_sql_manager.add_staff_information(self.name,
+                                                self.name + "123",
+                                                "really987HARD654password")
+
+        print("Welcome to {} !".format(self.name))
+        print("If you are part of the staff please insert 1")
+        print("If you want to shop here please insert 2")
+        print("If you want to leave please insert 3")
+
+        choice = input("+++")
+
+        if choice == "1":
+            if self.is_part_of_the_staff():
+                while True:
+                    execute = self.staff_menu()
+                    if not execute:
+                        break
+            else:
+                print("YOU SHALL NOT PASS")
+
+        elif choice == "2":
+            while True:
+                execute = self.buyer_menu()
+                if not execute:
+                    break
+
+        elif choice == "3":
+            print("you just left {}".format(self.name))
