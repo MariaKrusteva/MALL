9e39909153af07ce63ded0f85dbc4b64fccad675
Maria Krusteva <mimito_68@abv.bg>
2014-06-26 19:46:50 +0300

    Adding maps for the labyrinth game

---
+++ labyrinth_maps.py
@@ -0,0 +1,23 @@
+def level_one():
+    map = "S..~..\n.~....\n......\n..~..F"
+    return map
+
+
+def level_two():
+    map = "S.~~~~\n..~~~~\n....~~\n.....F"
+    return map
+
+
+def level_three():
+    map = "S....~~\n.....~~\n~~~..~~\n..~...F"
+    return map
+
+
+def level_four():
+    map = "S..~.~~.\n....~..~\n........\n...~~.~.\n~~....~~\n...~...F"
+    return map
+
+
+def level_five():
+    map = "S..~.~~.\n....~..~\n........\n...~~.~.\n......~~\n...~...F"
+    return map
